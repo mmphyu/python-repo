ff4efb7adc842e406735cfb95f7fce655e5f86dd
Mon Myat Phyu <monmyatphyu1996@gmail.com>
2021-08-27 15:58:45 +0630

    added type.py

---
+++ chapter_two/type.py
@@ -0,0 +1,10 @@
+# type(): a method to check type of a value
+
+print(type(20))
+
+print(type(28.3))
+
+print(type('A'))
+
+print(type("PYTHON"))
+
--- keywords.py
+++ keywords.py
@@ -1,3 +1,9 @@
-#Can check keywords in Python, which you cannot use as variable names.
+# The following reserved keywords in Python cannot be used as identifier (variable names).
 
 help("keywords")
+
+#OR
+
+import keyword
+
+keyword.kwlist
